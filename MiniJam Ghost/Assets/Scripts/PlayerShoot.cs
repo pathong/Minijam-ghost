918918475ee56082e3952b087d3f1daf4bddd6a6
Pathong <paopeaw8246@gmail.com>
2022-10-14 15:42:43 +0700

    Basic Gun Feature

--- MiniJam Ghost/Assets/Scripts/PlayerShoot.cs
+++ MiniJam Ghost/Assets/Scripts/PlayerShoot.cs
@@ -6,12 +6,39 @@ using UnityEngine.InputSystem;
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] private Transform _gunTip;
+    [SerializeField] private Transform _gunPivot;
     [SerializeField] private int _bulletAmount;
+    [SerializeField] private float _maxAngle;
+    [SerializeField] private GameObject _bulletPrefab;
 
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Shoot();
+        }
+    }
 
     public void Shoot()
     {
         Debug.Log("Shoot");
+        float spread = 1;
+        Quaternion newRot = _gunPivot.rotation;
+
+        for (int i = 0; i < _bulletAmount; i++)
+        {
+            float addedOffset = Random.Range(-_maxAngle, _maxAngle);
+
+
+            // Then add "addedOffset" to whatever rotation axis the player must rotate on
+            newRot = Quaternion.Euler(_gunPivot.transform.localEulerAngles.x,
+            _gunPivot.transform.localEulerAngles.y,
+            _gunPivot.transform.localEulerAngles.z + addedOffset);
+
+            GameObject bullet =  Instantiate(_bulletPrefab, _gunTip.position, newRot);
+
+
+        }
     }
 
 
