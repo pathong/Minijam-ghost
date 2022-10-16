1248edf2aa409d7bce9782533fa7027bc8343c85
Pathong <paopeaw8246@gmail.com>
2022-10-16 12:57:06 +0700

    Fix enemy test scene

--- MiniJam Ghost/Assets/Scripts/EnemyScript/Enemy.cs
+++ MiniJam Ghost/Assets/Scripts/EnemyScript/Enemy.cs
@@ -12,12 +12,13 @@ public abstract class Enemy : MonoBehaviour, IDamangable
     [SerializeField] protected float attackRange;
     [SerializeField] private float attackTime = 1f;
     [SerializeField] private AudioClip walkSound;
+    [SerializeField] private ParticleSystem smokePar;
     public int MaxHealth;
     private int currentHealth;
 
     private NavMeshAgent agent;
 
-    public GameObject player;
+    public Transform player;
 
     private bool isAttacking;
     
@@ -31,6 +32,15 @@ public abstract class Enemy : MonoBehaviour, IDamangable
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+
+        player = Extension.GetPlayer();
+
+    }
+
+    private void Start()
+    {
+        Instantiate(smokePar, new Vector2(transform.position.x, transform.position.y + 2), Quaternion.identity);
     }
 
 
@@ -42,9 +52,9 @@ public abstract class Enemy : MonoBehaviour, IDamangable
                 if(player!= null) { currentState = EnemyState.Chasing; StartCoroutine(nameof(WalkSound)); }
                 break;
             case EnemyState.Chasing:
-                agent.SetDestination(player.transform.position);
+                agent.SetDestination(player.position);
                 
-                if(Vector2.Distance(player.transform.position, this.transform.position) <= attackRange)
+                if(Vector2.Distance(player.position, this.transform.position) <= attackRange)
                 {
                     currentState = EnemyState.Attacking;
                     agent.ResetPath();
@@ -70,7 +80,7 @@ public abstract class Enemy : MonoBehaviour, IDamangable
     {
         isAttacking = false;
         // Check next state after attack animation is finished.
-        if(Vector2.Distance(player.transform.position, this.transform.position) > attackRange)
+        if(Vector2.Distance(player.position, this.transform.position) > attackRange)
         {
             currentState = EnemyState.Chasing;
             StartCoroutine(nameof(WalkSound));
@@ -95,7 +105,8 @@ public abstract class Enemy : MonoBehaviour, IDamangable
         currentHealth -= 1;
         if(currentHealth <= 0) {
             currentState = EnemyState.Dead;
-            Destroy(gameObject, 2f); 
+            Instantiate(smokePar, new Vector2(transform.position.x, transform.position.y + 2), Quaternion.identity);
+            Destroy(gameObject); 
         } //TODO : maybe do something else
     }
 
