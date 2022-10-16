6e11a7332d34da676ef823be944c0a021e387068
Pathong <paopeaw8246@gmail.com>
2022-10-16 15:26:07 +0700

    Spawn System

--- MiniJam Ghost/Assets/Scripts/Wave Script/WaveSpawn.cs
+++ MiniJam Ghost/Assets/Scripts/Wave Script/WaveSpawn.cs
@@ -6,8 +6,11 @@ using UnityEngine.Tilemaps;
 public class WaveSpawn : MonoBehaviour
 {
     static WaveSpawn waveSpawn;
-    [SerializeField] private Tilemap groundTilemap;
-    [SerializeField] private GameObject monsTest;
+    private Tilemap groundTilemap;
+
+    public GameObject enemyLv1;
+    public GameObject enemyLv2;
+
     private Transform player;
 
 
@@ -20,18 +23,16 @@ public class WaveSpawn : MonoBehaviour
     private void Awake()
     {
         if (waveSpawn == null) waveSpawn = this;
-    }
-
-    private void Start()
-    {
-        StartCoroutine(IntervalSpawn());
+        groundTilemap = GameObject.FindGameObjectWithTag("GroundTileMap").GetComponent<Tilemap>();
     }
 
 
 
-    public void NormalSpawn()
+
+    void NormalSpawn(GameObject monst = null)
     {
-        Instantiate(monsTest, GetSpawnPoint(), Quaternion.identity);
+        if(monst == null) { monst = enemyLv1; }
+        Instantiate(monst, GetSpawnPoint(), Quaternion.identity);
     }
 
     IEnumerator IntervalSpawn()
@@ -43,7 +44,7 @@ public class WaveSpawn : MonoBehaviour
         }
     }
 
-    public Vector2 GetSpawnPoint()
+    Vector2 GetSpawnPoint()
     {
         while (true)
         {
@@ -63,19 +64,55 @@ public class WaveSpawn : MonoBehaviour
     }
 
 
-    public static void SpawnWave(int amount)
+
+
+
+    /// <summary>
+    /// Spawn monster in wave
+    /// </summary>
+    /// <param name="amount">default value for 1 wave is 5 monster</param>
+    public static void SpawnWave(int amount = 5)
     {
+        Debug.Log("SpawnWave");
         for (int i = 0; i < amount; i++)
         {
             waveSpawn.NormalSpawn();
         }
         
     }
+     
+    /// <summary>
+    /// spawn monster once
+    /// </summary>
+    public static void SpawnNormal(GameObject monst = null)
+    {
+        Debug.Log("Spawn Normal");
+        waveSpawn.NormalSpawn(monst);
+    }
+    
+    /// <summary>
+    /// Start Interval Spawn
+    /// </summary>
+    public static void StartIntervalSpawn()
+    {
+        Debug.Log("Start Interval");
+        waveSpawn.StartCoroutine(waveSpawn.IntervalSpawn());
+    }
+    /// <summary>
+    /// Stop Interval Spawn
+    /// </summary>
+    public static void StopIntervalSpawn()
+    {
+        Debug.Log("Stop Interval");
+        waveSpawn.StopCoroutine(waveSpawn.IntervalSpawn());
+    }
 
-    [ContextMenu("SpawnInWave")]
+    
+    
+    [ContextMenu("Test SpawnInWave")]
     public void SpawnWaveTest()
     {
-        SpawnWave(5);
+        SpawnWave();
 
     }
 
